9a31d8eded058ffbac8a20d13c1dd4af87bc4e72
RuhanR-git <ruhan.raghu@gmail.com>
2026-03-25 16:30:49 -0600

    Made just drive base and path planner code
    Took bigbird code and removed every thing related to the shooter and intake, so it would match robot changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,19 +1,11 @@
 package frc.robot;
 
 // WPILIB and YAGSL imports
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
+//import edu.wpi.first.math.geometry.Pose2d;
+//import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.IntakeBall;
-import frc.robot.commands.ShootSequence;
-import frc.robot.commands.ShooterJiggle;
-import frc.robot.commands.StowIntake;
-import frc.robot.subsystems.HoodSubsystem;
-import frc.robot.subsystems.IntakeSubsystem;
-import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 import swervelib.SwerveInputStream;
 
@@ -21,14 +13,7 @@ public class RobotContainer {
 
   private final SwerveSubsystem m_swerveSubsystem = new SwerveSubsystem();
 
-  private final IntakeSubsystem m_intakeSubsystem = new IntakeSubsystem();
-
-  private final HoodSubsystem m_hoodSubsystem = new HoodSubsystem();
-
-  private final ShooterSubsystem m_shooterSubsystem = new ShooterSubsystem();
-
   private final CommandXboxController m_driverController = new CommandXboxController(OperatorConstants.kDriverControllerPort);
-  private final CommandXboxController m_operatorController = new CommandXboxController(OperatorConstants.kOperatorControllerPort);
 
   public RobotContainer() {
     // Note: Removed redundant NamedCommands.registerCommand here to prevent 
@@ -59,42 +44,8 @@ public class RobotContainer {
     m_swerveSubsystem.setDefaultCommand(m_swerveSubsystem.driveFieldOriented(driveInputStream));
     m_driverController.y().onTrue(m_swerveSubsystem.zeroGyroCommand());
     m_driverController.b().whileTrue(m_swerveSubsystem.lockPoseCommand());
-
-    // --- Operator bindings for intake (Operator controller)---
-
-    // Left Trigger -> Intake Balls
-    m_operatorController.leftTrigger()
-    .whileTrue(new IntakeBall(m_intakeSubsystem))
-    .onFalse(new StowIntake(m_intakeSubsystem));
-    
-    // Right Trigger -> The "Big Dumper" Shooting Logic
-m_operatorController.rightTrigger()
-    .whileTrue(
-        //Start the Jiggle
-        new ShooterJiggle(m_intakeSubsystem)
-        //Simultaneously aim the robot and run the shoot sequence
-        .alongWith(
-            Commands.runOnce(() -> {
-                Pose2d targetPose = new Pose2d(SelectHub.hubPosition(), new Rotation2d());
-                SwerveInputStream angleDriveInputStream = driveInputStream.copy().aim(targetPose);
-                m_swerveSubsystem.setDefaultCommand(m_swerveSubsystem.driveFieldOriented(angleDriveInputStream));
-            }),
-            // This replaces the deprecated .schedule() call
-            new ShootSequence(m_shooterSubsystem, m_hoodSubsystem, () -> m_swerveSubsystem.getPose())
-        )
-    )
-    .onFalse(
-        // When let go: Stow the intake and reset the drive to normal field-oriented
-        new StowIntake(m_intakeSubsystem)
-            .alongWith(Commands.runOnce(() -> 
-                m_swerveSubsystem.setDefaultCommand(m_swerveSubsystem.driveFieldOriented(driveInputStream))
-            ))
-    );
-
     // Apply the drive command as the default
     m_swerveSubsystem.setDefaultCommand(m_swerveSubsystem.driveFieldOriented(driveInputStream));
-    m_shooterSubsystem.setDefaultCommand(m_shooterSubsystem.run(() -> m_shooterSubsystem.stop()));
-    m_intakeSubsystem.setDefaultCommand(new StowIntake(m_intakeSubsystem));
   }
 
   /**
