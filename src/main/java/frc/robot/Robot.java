17459d62373756b116a579fd6464e4dceb4b3eb3
RuhanR-git <ruhan.raghu@gmail.com>
2026-04-09 15:01:34 -0600

    Problems

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -29,8 +29,6 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    m_robotContainer.setupTeleopHeading();
-
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
